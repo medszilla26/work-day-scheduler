d2b75f30fabf7e4d6c3e1f93c9d3ea946ccc9db0
M3D5 <pablohdzjr@gmail.com>
2020-05-11 22:33:25 -0500

    saveBtn works using jQuery

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,6 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-/*function currentTime(time) {
-  var hours = time.getHours();
-  var ampm = hours >= 12 ? "pm" : "am";
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  var strTime = hours + ampm;
-  return strTime;
-}*/
+function saveDesc() {}
+
+$(".saveBtn").click(saveDesc);
+console.log("works");
